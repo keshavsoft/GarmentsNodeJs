efa7d414ff6d171e435a70014f5d3ff9180cbc13
nknnkumar <115708070+nknnkumar@users.noreply.github.com>
2022-12-31 11:33:02 +0530

    keshav

--- public/pages/QrCodes/Generation/ShowAll/AddListeners.js
+++ public/pages/QrCodes/Generation/ShowAll/AddListeners.js
@@ -3,7 +3,7 @@ import { StartFunc as DeleteFuncsStartFunc } from "./ButtonFuncs/DeleteFuncs.js"
 
 let StartFunc = ({ inFolderName, inFileName, inItemName, inProjectName }) => {
     let jVarLocalDeleteButtons = document.getElementsByClassName("DeleteButtonClass");
-
+  //  console.log("jVarLocalDeleteButtons : ", jVarLocalDeleteButtons);
     for (var i = 0; i < jVarLocalDeleteButtons.length; i++) {
         jVarLocalDeleteButtons[i].addEventListener("click", async (event) => {
             await DeleteFuncsStartFunc({
