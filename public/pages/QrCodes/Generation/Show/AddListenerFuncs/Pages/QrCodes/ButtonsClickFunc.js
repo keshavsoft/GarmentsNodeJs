6450da1e678afbd68148db03d686632e6dfdaea8
Srivasu215 <106431000+Srivasu215@users.noreply.github.com>
2023-01-07 11:12:11 +0530

    SREENIVAS

---
+++ public/pages/QrCodes/Generation/Show/AddListenerFuncs/Pages/QrCodes/ButtonsClickFunc.js
@@ -0,0 +1,48 @@
+let StartFunc = () => {
+    let jVarLocalShowQrCodeButtonId = document.getElementById("GenerateQrCodeButtonId");
+
+    jVarLocalShowQrCodeButtonId.addEventListener("click", jFShowQrCodeButtonClickFunc);
+
+
+};
+
+let jFShowQrCodeButtonClickFunc = () => {
+    let jVarLocalDivToShowQrCodeId = document.getElementById("DivToShowQrCodeId");
+    GenerateQrCodeOnModal({ inQrData: "16", inCanvasId: jVarLocalDivToShowQrCodeId });
+    console.log("ssssssss");
+};
+
+let GenerateQrCodeOnModal = ({ inQrData = "", inCanvasId }) => {
+    var canvas = inCanvasId;
+    canvas.height = 1;
+    canvas.width = 1;
+    canvas.style.visibility = 'hidden';
+
+    // Convert the options to an object.
+    let opts = {};
+
+    // Finish up the options
+    opts.text = inQrData;
+    opts.bcid = "qrcode";
+    opts.scaleX = 1;
+    opts.scaleY = 1;
+    opts.rotate = "N";
+
+    // Draw the bar code to the canvas
+    try {
+        let ts0 = new Date;
+        bwipjs.toCanvas(canvas, opts);
+        show(ts0, new Date);
+    } catch (e) {
+        console.log("error : ", e);
+
+        return;
+    }
+
+    function show(ts0, ts1) {
+        canvas.style.visibility = 'visible';
+    }
+};
+
+
+export { StartFunc };
