12897811c989509b4c8b5e3fdabbeff8b51425b4
Keshav <nknnkumar@yahoo.com>
2023-01-19 20:22:47 +0530

    keshav

--- public/pages/QrCodes/Generation/CreateNew/Items/ShowOnDom.js
+++ public/pages/QrCodes/Generation/CreateNew/Items/ShowOnDom.js
@@ -1,25 +1,21 @@
 import { StartFunc as FetchFunc } from "./FetchFunc.js";
 
 let StartFunc = async ({ inProjectName }) => {
-  let jVarLocalFromFetch = await FetchFunc({ inProjectName });
+    let jVarLocalFromFetch = await FetchFunc({ inProjectName });
 
-  if (jVarLocalFromFetch.KTF) {
-    let jVarLocalItemsDataListId = document.getElementById("ItemsDataListId");
+    if (jVarLocalFromFetch.KTF) {
+        let jVarLocalItemsDataListId = document.getElementById("ItemsDataListId");
 
-    if (jVarLocalItemsDataListId !== null) {
-      var options = '';
+        if (jVarLocalItemsDataListId !== null) {
+            var options = '';
 
-      for (var i = 0; i < jVarLocalFromFetch.JsonData.length; i++) {
-        options += `<option value="${jVarLocalFromFetch.JsonData[i].ItemName}">${jVarLocalFromFetch.JsonData[i].ItemName}</option>`;
-      }
+            for (var i = 0; i < jVarLocalFromFetch.JsonData.length; i++) {
+                options += `<option value="${jVarLocalFromFetch.JsonData[i].ItemName}">${jVarLocalFromFetch.JsonData[i].ItemName}</option>`;
+            }
 
-      jVarLocalItemsDataListId.innerHTML = options;
-
-    }
-
-
-
-  };
+            jVarLocalItemsDataListId.innerHTML = options;
+        };
+    };
 };
 
 export { StartFunc };
