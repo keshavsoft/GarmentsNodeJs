ae06a77aaa96b5e81aedd4648cb34174abc8efd8
keshavsoft <nknnkumar@yahoo.com>
2023-03-24 18:14:52 +0530

    keshav

--- public/pages/QrCodes/WithPurchaseDetails/Show/AddListenerFuncs/StartFuncs.js
+++
@@ -1,8 +0,0 @@
-//import { StartFunc as ButtonsClickFuncStartFunc } from "./Pages/QrCodes/ButtonsClickFunc.js";
-import { StartFunc as ButtonsClickFuncStartFunc } from "./Pages/QrCodes/ButtonsClickFuncs/ToModal.js";
-
-let StartFunc = () => {
-    ButtonsClickFuncStartFunc();
-};
-
-export { StartFunc };
--- public/pages/QrCodes/WithPurchaseDetails/Show/urlSearchParams.js
+++
@@ -1,9 +0,0 @@
-let ReturnRowPK = () => {
-    let jVarLocalSearch = document.location.search;
-    const params = new URLSearchParams(jVarLocalSearch);
-    const RowPK = params.get("RowPK");
-
-    return { RowPK };
-};
-
-export { ReturnRowPK }
